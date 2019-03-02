80b7648eab5990398f2bbc8f2a6976a7b6e9007b
David Omenyo <davidomenyo@gmail.com>
2019-03-02 20:47:20 +0000

    stopwatch complete

--- task2/stopwatch.js
+++ task2/stopwatch.js
@@ -6,75 +6,6 @@ $(() => {
     let ms = 0;
 
     $("#display").append("0" + hrs + ":" + "0" + mins + ":" + "0" + seconds + ":" + "0" + ms);
-    function increment(){
-        ms++;
-    }
-
-    //TODO: think of putting increments at the end of the functions.
-    function secondsTick(){
-        setInterval(() => {
-            seconds++;
-            if(seconds < 10){
-                $(".clock #seconds").html("0" + seconds + ":");
-            } else{
-                $(".clock #seconds").html(seconds + ":");
-            }
-            if(seconds >= 59){
-                seconds = 0;
-            }
-        }, 1000);
-    }
-
-    function millisecondsTick(){
-        setInterval(() => {
-            ms++;
-            if(ms < 10){
-                $(".clock #milliseconds").html("0" + ms);
-            } else{
-                $(".clock #milliseconds").html(ms);
-            }
-
-            if(ms > 99){
-                ms = 0;
-            }
-        }, 1);
-    }
-
-    function minutes(){
-        setInterval(() => {
-            mins++;
-            if(mins < 10){
-                $(".clock #minutes").html("0" + mins + ":");
-            } else{
-                $(".clock #minutes").html(mins + ":");
-            }
-
-            if(mins >= 59){
-                mins = 0;
-            }
-        }, 60000);
-    }
-
-    function hours(){
-        if(mins === 60){
-            if(hrs < 10){
-                $(".clock #hours").html("0" + hrs + ":");
-            } else{
-                $(".clock #hours").html(hrs + ":");
-            }
-            if(hrs >= 23){
-                hrs = 0;
-            }
-            hrs++;
-        }
-    }
-
-    //TODO: apply these to the buttons using .on();
-    // secondsTick();
-    // millisecondsTick();
-    // minutes();
-    // hours();
-
 
     let stopMs;
     let stopSec;
@@ -135,7 +66,6 @@ $(() => {
         }
     }
 
-    // watch();
     $("#start").on('click', watch);
     $("#pause").on('click', () => {
         clearInterval(stopMs);
@@ -147,5 +77,20 @@ $(() => {
        $(".clock #minutes").html("0" + 0 + ":");
        $(".clock #seconds").html("0" + 0 + ":");
        $(".clock #milliseconds").html("0" + 0);
+    });
+
+    let count = 0;
+    $("#lap").on('click', () => {
+        let hrsID = $("#hours").html();
+        let minsID = $("#minutes").html();
+        let secID = $("#seconds").html();
+        let msID = $("#milliseconds").html();
+        count++;
+        $("#laps").append("<tr>"+ "<td>" + count + "</td>" + "<td>"+ hrsID + minsID + secID + msID +"</td>" + "</tr>");
+    });
+    $("#clear").on('click', () => {
+        let table = $("#laps").html();
+        $("td").remove();
+        count = 0;
     });
 });

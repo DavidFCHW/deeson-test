1a99d86585db553ad7cf033592ad576454cec86e
David Omenyo <davidomenyo@gmail.com>
2019-03-03 20:40:48 +0000

    completed bonus functionality

--- task2/stopwatch.js
+++ task2/stopwatch.js
@@ -1,10 +1,29 @@
 $(() => {
+
     let hrs = 0;
     let mins = 0;
     let seconds = 0;
     let ms = 0;
 
     $("#display").append("0" + hrs + ":" + "0" + mins + ":" + "0" + seconds + ":" + "0" + ms);
+
+    let sessKeys = [];
+    for(let k = 0; k < sessionStorage.length; k++){
+        sessKeys.push(parseInt(sessionStorage.key(k)));
+    }
+    sessKeys.sort();
+    console.log(sessKeys[1]);
+    for(let i = 0; i < sessionStorage.length; i++){
+        if(sessKeys[i] < 10) {
+            $("#laps").append("<tr>" + "<td>" + sessKeys[i] + "</td>" + "<td>" + sessionStorage.getItem(sessKeys[i].toString()) + "</td>" + "</tr>");
+        }
+    }
+    for(let i = 0; i < sessionStorage.length; i++){
+        if(sessKeys[i] >= 10) {
+            $("#laps").append("<tr>" + "<td>" + sessKeys[i] + "</td>" + "<td>" + sessionStorage.getItem(sessKeys[i].toString()) + "</td>" + "</tr>");
+        }
+    }
+
 
     let stopMs;
     let stopSec;
@@ -90,10 +109,15 @@ $(() => {
         let msID = $("#milliseconds").html();
         count++;
         $("#laps").append("<tr>"+ "<td>" + count + "</td>" + "<td>"+ hrsID + minsID + secID + msID +"</td>" + "</tr>");
+
+        sessionStorage.setItem(count.toString(), hrsID + minsID + secID + msID);
+
     });
     $("#clear").on('click', () => {
         let table = $("#laps").html();
         $("td").remove();
         count = 0;
+        sessionStorage.clear();
     });
+
 });
